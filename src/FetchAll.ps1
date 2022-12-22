2ef63f0ee524f030e9356e5c6aae83d124ab3ebe
KeasbeyNights <king_89@hotmail.co.uk>
2022-12-22 11:56:19 +0000

    Updated fetchall progress message

--- src/FetchAll.ps1
+++ src/FetchAll.ps1
@@ -1,12 +1,11 @@
 function FetchAll() {
     $env:GitRepo = [Environment]::GetEnvironmentVariable('GitRepo', 'User')
-    Write-Progress 'Fetching all repos in' $env:GitRepo
-    $i = 0    
+    $i = 0
 
     $directories = Get-ChildItem –Path $env:GitRepo -Directory
     foreach ($directory in $directories) {
-        git -C $directory fetch;
         $i++
-        Write-Progress -activity "Fetching all repos..." -status "Fetching: $directory.Name ($i of $($directories.Count))" -percentComplete (($i / $directories.Count) * 100)
+        Write-Progress -activity "Fetching all repos..." -status "Fetching: $($directory.Name) ($i of $($directories.Count))" -percentComplete (($i / $directories.Count) * 100)
+        git -C $directory fetch;
     }    
 }
